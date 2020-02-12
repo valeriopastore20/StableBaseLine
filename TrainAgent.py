5cc2ea27574ecd787326a2b44b59f43ffa534bf0
valerio <valeriopastore20@gmail.com>
2020-02-12 17:58:40 +0100

    s

--- TrainAgent.py
+++ TrainAgent.py
@@ -18,11 +18,13 @@ def callback(locals_, globals_):
     self_ = locals_['self']
     global num_steps
     if (num_steps+1) % 1000 == 0:
-    	current_impr = (env.initial_sum-env.current_sum)/env.initial_sum*100
-    #mff_impr =(env.initial_sum-env.mff_sum)/env.initial_sum*100
-    #value = current_impr - mff_impr
-    	summary = tf.Summary(value=[tf.Summary.Value(tag='improvement', simple_value=current_impr)])
-    	locals_['writer'].add_summary(summary, self_.num_timesteps)
+        current_impr = (env.initial_sum-env.current_sum)/env.initial_sum*100
+        mff_impr =(env.initial_sum-env.mff_sum)/env.initial_sum*100
+        over_mff = current_impr - mff_impr
+        summary = tf.Summary(value=[tf.Summary.Value(tag='improvement', simple_value=current_impr)])
+        summary2 = tf.Summary(value=[tf.Summary.Value(tag='improvement over mff', simple_value=over_mff)])
+        locals_['writer'].add_summary(summary, self_.num_timesteps)
+        locals_['writer'].add_summary(summary2, self_.num_timesteps)
     num_steps+=1
     return True
 
