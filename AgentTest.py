be0594d45362eba8db86f0be1e9d068cc99b5246
valeriopastore20 <valeriopastore20@gmail.com>
2020-04-02 16:24:59 +0200

    Add files via upload

---
+++ AgentTest.py
@@ -0,0 +1,27 @@
+from stable_baselines.common.env_checker import check_env
+import gym
+import gym_qap
+import gym_qapImg
+import gym_qapConst
+import gym_qapImgConst
+
+
+from stable_baselines import DQN
+from stable_baselines import PPO2
+from stable_baselines.common.evaluation import evaluate_policy
+
+env = gym.make('qapImgConst-v0')
+# It will check your custom environment and output additional warnings if needed
+check_env(env)
+
+
+#If you want to quickly try a random agent on your environment, you can also do:
+
+obs = env.reset()
+n_steps = 10
+for _ in range(n_steps):
+    # Random action
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+
+print("DONE")
---
+++ UseAgent.py
@@ -0,0 +1,61 @@
+import gym
+import gym_qap
+import gym_qapConst
+import gym_qapImg
+import gym_qapImgConst
+import matplotlib.pyplot as plt
+import os
+import argparse
+
+from stable_baselines import DQN
+from stable_baselines import A2C
+from stable_baselines import PPO2
+from stable_baselines.common.evaluation import evaluate_policy
+
+"""
+	Questo programma consente di testare un agente gia' allenato e crea
+	un grafico dei risultati ottenuti nelle varie simulazioni rispetto
+	al risultato ottenuto adottando la disposizione mff
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("num_prod", type=int, help="Numero di prodotti su cui il modello e' stato allenato")
+parser.add_argument("num_sim", type=int, help="Numero di simulazioni da effettuare")
+parser.add_argument("model_name", type=str, help="Nome del modello da testare")
+parser.add_argument("env", type=str, help="Nome dell'environment: Const lo stato iniziale e' fisso, \
+    Img l'osservazione e' basata sull'immagine ", choices = ['qapConst-v0', 'qapImgConst-v0', 'qap-v0', 'qapImg-v0'])
+args = parser.parse_args()
+
+env = gym.make(args.env)
+
+
+# Load the trained agent
+path = os.getenv("HOME")+"/models/"+args.model_name
+model = DQN.load(path)
+model.set_env(env)
+
+# Enjoy trained agent
+num_simulations = args.num_sim
+x = [i for i in range(0,num_simulations)] 
+y = [0 for j in range(0,num_simulations)]
+
+for j in range(num_simulations):
+	obs = env.reset()
+	while not env.done:
+		action, _states = model.predict(obs)
+		obs, rewards, dones, info = env.step(action)
+	y[j] = ((env.initial_sum-env.final_sum)/env.initial_sum*100)-((env.initial_sum-env.mff_sum)/env.initial_sum*100)
+
+# plotting the points  
+plt.plot(x,y, label = "Agent improvement over mff") 
+
+# naming the x axis 
+plt.xlabel('Simulation') 
+# naming the y axis 
+plt.ylabel('% improvement ') 
+  
+# giving a title to my graph 
+plt.title('Imrovement Graph') 
+  
+# function to show the plot 
+plt.show() 
